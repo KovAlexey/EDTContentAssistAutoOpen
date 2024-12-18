ee4754fb47cab8e5e87e2a3de9261b46c5c5f3b3
KovAlexey <kovalexei@gmail.com>
2024-12-18 17:28:37 +1000

    test

--- bundles/org.kovalexey.edt.contentassist.autoopen/src/org/kovalexey/edt/autoopenpropossal/Activator.java
+++ bundles/org.kovalexey.edt.contentassist.autoopen/src/org/kovalexey/edt/autoopenpropossal/Activator.java
@@ -3,7 +3,11 @@ package org.kovalexey.edt.autoopenpropossal;
 import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.PreferenceStore;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IPageListener;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import org.osgi.framework.BundleContext;
@@ -35,7 +39,22 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	public void init() {
-		PlatformUI.getWorkbench().addWindowListener(ListenersImpl.windowsListener);
+		
+		Display display = Display.getDefault();
+		if (display != null) {
+			display.syncExec(new Runnable() {
+				
+				@Override
+				public void run() {
+					IWorkbenchWindow[] windows = PlatformUI.getWorkbench().getWorkbenchWindows();
+					for (IWorkbenchWindow iWorkbenchWindow : windows) {
+						iWorkbenchWindow.getPartService().addPartListener(ListenersImpl.PartOpenedListener);
+					}
+					PlatformUI.getWorkbench().addWindowListener(ListenersImpl.windowsListener);
+				}
+			});
+		}
+	
 	}
 	
 	@Override
